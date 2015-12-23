4f92786ceddc729bd33f79d17a0b38c739ceca46
Gonzalo Pozzo <gonzalo.pozzo@etermax.com>
2015-12-23 12:55:57 -0300

    Implementado borrado

---
+++ src/js/partials/extras/loadedMarkerSolucionable.js
@@ -0,0 +1,19 @@
+google.maps.event.addListener(marker, 'rightclick', function() {
+
+    {% if opciones.markers.maximosRemove %}
+      //Max markers remove
+      {% include "../options/maxMarkersRemove.js" %}
+    {% endif %}
+
+    infowindow.close();
+    if (marker.solucionado == false) {
+        marker.setIcon('img/icons/tilde.png');
+        marker.solucionado = true;
+        solucionados.push(marker);
+        cant_solucionados++;
+    } else {
+        marker.setIcon(marker.iconBu);
+        marker.solucionado = false;
+        cant_solucionados--;
+    }
+});
