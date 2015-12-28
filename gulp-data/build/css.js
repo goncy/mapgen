3d4498c72dcdbc449f814548a3a4c199d1fcf642
Gonzalo Pozzo <gonzalo.pozzo@etermax.com>
2015-12-28 18:04:35 -0300

    Arreglado nav collapse

--- gulp-data/build/css.js
+++ gulp-data/build/css.js
@@ -9,12 +9,11 @@ var gulp = require('gulp'),
 
 var cssVendors = {
   toastr: "node_modules/toastr/toastr.scss",
-  bootstrap: "node_modules/bootstrap-sass/assets/stylesheets/bootstrap.scss",
   fontAwesome: "node_modules/font-awesome/css/font-awesome.css"
 }
 
 gulp.task('build:css', function() {
-  return gulp.src(['./src/{sass,css}/*.{scss,css}', cssVendors.toastr, cssVendors.bootstrap, cssVendors.fontAwesome])
+  return gulp.src(['./src/{sass,css}/*.{scss,css}', cssVendors.toastr, cssVendors.fontAwesome])
     .pipe(sass())
     .pipe(concat('gpozzo.css'))
     .pipe(gulpif(argv.env === "prod", minifyCss()))
