1c368fe11d6dc9f8775476594124ba8fb55d3910
Gonzalo Pozzo <gonzalo.pozzo@etermax.com>
2015-12-29 14:43:58 -0300

    Nuevos features

--- src/js/partials/extras/filtrar.js
+++ src/js/partials/extras/filtrar.js
@@ -4,9 +4,9 @@
 
       var desde = $('#filtro-desde').val();
       var hasta = $('#filtro-hasta').val();
-      var categorias = $('#filtro-categorias').val();
+      var catFiltradas = $('#filtro-categorias').val();
 
-      if (!desde || !hasta || !categorias) {
+      if (!desde || !hasta || !catFiltradas) {
           alert('Marque fecha de inicio, final y categorias');
           return;
       }
@@ -14,7 +14,7 @@
       for (var cat in markerContainer) {
         for (var mk in markerContainer[cat]) {
           var estaEnFecha = markerContainer[cat][mk].fecha <= hasta && markerContainer[cat][mk].fecha >= desde ? true : false;
-          var estaEnCategoria = categorias.indexOf(markerContainer[cat][mk].tipo) >= 0 ? true : false;
+          var estaEnCategoria = catFiltradas.indexOf(markerContainer[cat][mk].tipo) >= 0 ? true : false;
           if (!estaEnFecha || !estaEnCategoria){
               markerContainer[cat][mk].setMap(null);
           }
--- src/js/partials/variables.js
+++ src/js/partials/variables.js
@@ -1,9 +1,10 @@
 //Cargamos las categorias desde nunjucks
-var categorias = [{% for categoria in categorias -%} '{{ categoria }}' {{"" if loop.last else ","}} {%- endfor %}];
+var categorias = [{% for categoria in categorias -%}{ label: '{{categoria.label}}', value: '{{categoria.value}}'} {{"" if loop.last else ","}} {%- endfor %}];
+var listaCategorias = [];
 var markerContainer = {};
 
 {% if extras.editable %}
-  var itemSeleccionado = categorias[0];
+  var itemSeleccionado = categorias[0].label;
   var cant_markers = 0;
 
   {% if extras.solucionable %}
