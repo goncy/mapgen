316975163270676fdb77048a2394ea86647ad6c8
gonzalo.pozzo <gonzalo.pozzo@etermax.com>
2016-01-27 17:24:21 -0300

    Nuevo sistema de como llegar

--- src/js/partials/extras/comoLlegar.js
+++ src/js/partials/extras/comoLlegar.js
@@ -1,15 +1,26 @@
-{% if extras.comoLlegar %}
-  function comoLlegar(lat,lng){
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        $.get('https://maps.googleapis.com/maps/api/directions/json?origin='+position.coords.latitude+','+position.coords.longitude+'&destination='+lat+','+lng, function(data) {
-          if(data.status === "OK"){
-            var recorrido = google.maps.geometry.encoding.decodePath(data.routes[0].overview_polyline.points);
-            if(recorrido.length){
+{ % if extras.comoLlegar % }
+
+function comoLlegar(lat, lng) {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+
+      var directionsService = new google.maps.DirectionsService;
+      var directionsDisplay = new google.maps.DirectionsRenderer;
+
+      directionsDisplay.setMap(mapa);
+
+      directionsService.route({
+        origin: position.coords.latitude + ',' + position.coords.longitude,
+        destination: lat + ',' + lng,
+        travelMode: google.maps.TravelMode.DRIVING
+      }, function(data, status) {
+        if (status === google.maps.DirectionsStatus.OK) {
+            var recorrido = google.maps.geometry.encoding.decodePath(data.routes[0].overview_polyline);
+            if (recorrido.length) {
               var ln = new google.maps.Polyline({
                 path: recorrido,
                 geodesic: true,
-                strokeColor: '#'+(Math.random()*0xFFFFFF<<0).toString(16),
+                strokeColor: '#' + (Math.random() * 0xFFFFFF << 0).toString(16),
                 strokeOpacity: 1.0,
                 strokeWeight: 8,
                 zIndex: 9999,
@@ -24,31 +35,30 @@
 
               var latlngbounds = new google.maps.LatLngBounds();
               for (var i = 0; i < recorrido.length; i++) {
-                  latlngbounds.extend(recorrido[i]);
+                latlngbounds.extend(recorrido[i]);
               }
 
-              google.maps.event.addListener(ln, 'dblclick', function(){
-                  ln.setMap(null);
-                  mk.setMap(null);
-                  google.maps.event.clearInstanceListeners(ln);
+              google.maps.event.addListener(ln, 'dblclick', function () {
+                ln.setMap(null);
+                mk.setMap(null);
+                google.maps.event.clearInstanceListeners(ln);
               });
 
               mapa.fitBounds(latlngbounds);
 
-              if(!avisoBorrado){
+              if (!avisoBorrado) {
                 toastr.info("Para borrar/quitar un recorrido hacia un punto, presione dos veces sobre el.")
                 avisoBorrado = true;
               }
             }
-          }else{
+          } else {
             toastr.info("Hubo un error obteniendo el recorrido, por favor, intente nuevamente mas tarde");
           }
-        }, "json");
-      })
-    }else{
-      toastr.info("Esta opcion esta habilitada solamente con permisos GPS, por favor permitalo/habilitelo e intente nuevamente");
-    }
-
-    infowindow.close();
+      });
+    });
+  } else {
+    toastr.info("Esta opcion esta habilitada solamente con permisos GPS, por favor permitalo/habilitelo e intente nuevamente");
   }
-{% endif %}
+
+  infowindow.close();
+} { % endif % }
