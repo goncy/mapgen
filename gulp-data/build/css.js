3ab83afdd9a59e05c5b2284e7cd6064302180baf
gonzalo.pozzo <gonzalo.pozzo@etermax.com>
2016-04-01 09:34:40 -0300

    Se pueden cambiar algunos colores

--- gulp-data/build/css.js
+++ gulp-data/build/css.js
@@ -2,6 +2,9 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     concat = require('gulp-concat'),
     minifyCss = require('gulp-cssnano'),
+    nunjucksRender = require('gulp-nunjucks-render'),
+    _ = require('lodash'),
+    data = require('gulp-data'),
     gulpif = require('gulp-if'),
     argv = require('yargs').default('cliente', 'default').argv,
     templateData = require('../../builds/' + argv.cliente + '/build.json');
@@ -13,14 +16,20 @@ var cssVendors = {
 
 gulp.task('build:css', function() {
     if (templateData.admin.panel) {
-        gulp.src(['./src/{sass,css}/*.{scss,css}', cssVendors.toastr, cssVendors.fontAwesome])
+        var adminTemplateData = _.cloneDeep(templateData);
+        _.assignIn(adminTemplateData.extras, adminTemplateData.admin.extras);
+        gulp.src(['./src/{sass,css}/**.{scss,css}', cssVendors.toastr, cssVendors.fontAwesome])
+            .pipe(data(adminTemplateData))
+            .pipe(nunjucksRender({ ext: '.scss' }))
             .pipe(sass())
             .pipe(concat('gpozzo.css'))
             .pipe(gulpif(argv.env === "prod", minifyCss()))
             .pipe(gulp.dest('./dist/admin/css/'));
     }
 
-    return gulp.src(['./src/{sass,css}/*.{scss,css}', cssVendors.toastr, cssVendors.fontAwesome])
+    return gulp.src(['./src/{sass,css}/**.{scss,css}', cssVendors.toastr, cssVendors.fontAwesome])
+        .pipe(data(templateData))
+        .pipe(nunjucksRender({ ext: '.scss' }))
         .pipe(sass())
         .pipe(concat('gpozzo.css'))
         .pipe(gulpif(argv.env === "prod", minifyCss()))
